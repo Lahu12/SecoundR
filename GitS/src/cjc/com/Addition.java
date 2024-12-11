cc4d46786c91832bfbfad1f849cadbc9f258fc01
Admin <Admin@DESKTOP-JL8DKM2>
2024-12-11 18:59:06 +0530

    By addition

---
+++ GitS/src/cjc/com/Addition.java
@@ -0,0 +1,14 @@
+package cjc.com;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=40;
+		int c;
+		c=a+b;
+		System.out.println("Addition "+c);
+
+	}
+
+}
